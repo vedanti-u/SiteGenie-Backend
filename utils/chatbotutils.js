a0710059ea6e26c4ee37400138ddf2e4cb5ca0c4
Vedanti.U <vedanti.vvu@gmail.com>
2023-11-15 23:33:21 +0530

    fixed hnswlib implemented supabase

--- utils/chatbotutils.js
+++ utils/chatbotutils.js
@@ -1,26 +1,51 @@
 const fs = require('fs/promises');
 const path = require('path');
 const dotenv = require('dotenv');
+dotenv.config();
 const { compile } = require("html-to-text");
+//const { HierarchicalNSW } = require("hnswlib-node");
+
 const { RecursiveUrlLoader } = require("langchain/document_loaders/web/recursive_url");
-const { HNSWLib } = require('langchain/vectorstores/hnswlib');
-const {HierarchicalNSW} = require('hnswlib-node').HierarchicalNSW;
+//const { HNSWLib } = require('langchain/vectorstores/hnswlib');
+
 const { RetrievalQAChain } = require('langchain/chains');
 const { OpenAIEmbeddings } = require('langchain/embeddings/openai');
 const { OpenAI } = require('langchain/llms/openai');
-dotenv.config();
+//supabase imports
+const { SupabaseVectorStore } = require("langchain/vectorstores/supabase");
+const { createClient } = require("@supabase/supabase-js");
+const { RecursiveCharacterTextSplitter } = require('langchain/text_splitter');
+
+
 //const url = "https://daywiseai.com"
 //const VECTOR_STORE_PATH = `vector/${removeProtocol(url)}.index`;
+const privateKey = process.env.PUBLIC_SUPABASE_PRIVATE_KEY;
+    if (!privateKey) throw new Error(`Expected env var SUPABASE_PRIVATE_KEY`);
+    const supabase_url = process.env.PUBLIC_SUPABASE_URL;
+    if (!supabase_url) throw new Error(`Expected env var SUPABASE_URL`);
+    const client = createClient(supabase_url, privateKey);
+
 const model = new OpenAI({});
+
 
 async function createChatBot(url,prompt){
     //let vectorStore;
+    //supabase client
+    // const privateKey = process.env.PUBLIC_SUPABASE_PRIVATE_KEY;
+    // if (!privateKey) throw new Error(`Expected env var SUPABASE_PRIVATE_KEY`);
+    // const supabase_url = process.env.PUBLIC_SUPABASE_URL;
+    // if (!supabase_url) throw new Error(`Expected env var SUPABASE_URL`);
+    // const client = createClient(supabase_url, privateKey);
     console.log('In createChatBot func');
-    const VECTOR_STORE_PATH = `vector/${removeProtocol(url)}.index`;
-    const fileExists = await checkFileExists(VECTOR_STORE_PATH);
+    const url_to_check = `${removeProtocol(url)}`;
+    const fileExists = await checkFileExists(url_to_check);
     if(fileExists){
         console.log('Vector Exists');
-        const vectorStore = await HNSWLib.load(VECTOR_STORE_PATH,new OpenAIEmbeddings({ maxConcurrency: 5 }));
+        const vectorStore = await SupabaseVectorStore.fromExistingIndex(new OpenAIEmbeddings({ maxConcurrency: 5 }),{
+            client,
+            tableName: "documents",
+          });
+          console.log("this is vectorStore",vectorStore)
         const res = processPrompt(model,vectorStore,prompt);
         console.log("this is response in if=>",res);
     }
@@ -32,19 +57,65 @@ async function createChatBot(url,prompt){
             maxDepth:1,
             excludeDirs:["https://js.langchain.com/docs/api/"],
         });
-        const docs = await loader.load();
-        const vectorStore = await HNSWLib.fromDocuments(docs,new OpenAIEmbeddings({ maxConcurrency: 5 }));
-        await vectorStore.save(VECTOR_STORE_PATH);
-        const res = processPrompt(model,vectorStore);
+        const vecdocs = await loader.load();
+        const vecdocsArray = [vecdocs[0].pageContent]
+        console.log("vecdocs..",vecdocs)
+        const textSplitter = new RecursiveCharacterTextSplitter({
+            chunkSize: 1000,
+          });
+          const docs = await textSplitter.createDocuments(vecdocsArray);
+
+          
+          const vectorStore = new SupabaseVectorStore(new OpenAIEmbeddings({ maxConcurrency: 5 }), {
+            client,
+            tableName: "documents",
+          });
+          const website_url = `${removeProtocol(url)}`;
+          console.log("vectorstore...",vectorStore);
+          docs[0].metadata={"url":website_url};
+         const ids =  await vectorStore.addDocuments(docs);
+         console.log("This are ids: ",ids);
+        const res = processPrompt(model,vectorStore,prompt);
         console.log("this is response in else =>",res);
     }
-    return res;
+    //return res;
 }
-async function checkFileExists(filePath){
+// router.get('/', async (req, res) => {
+//     try {
+//       console.log(" Supabase query==>");
+//       const { data: Shoes, error } = await sql.from('Shoes').select('*')
+    
+//       console.log("Data:", Shoes);
+//       if (error) {
+//         console.log("Error:", error);
+//         return res.status(500).send({ error });
+//       }
+//       return res.send(Shoes);
+//     } catch (error) {
+//       console.log("Catch block error:", error);
+//       return res.status(500).send({ error });
+//     }
+//   });
+  
+
+async function checkFileExists(urlToCheck){
     try{
-        await fs.access(filePath);
         console.log("in try... checking if file exist")
-        return true;
+        console.log("supabase query..",urlToCheck)
+        var {data,error}=await client.from('documents').select('metadata');
+        console.log(data);
+
+        data = data.filter(e=>e.metadata.url=='daywiseai.com');
+        console.log("SSS",data);
+
+        if(error){
+            console.error("supabase error to find..",error);
+            return false;
+        }
+        const urlExists = data && data.length>0;
+        //await fs.access(filePath);
+      console.log("this is urlExists",urlExists);
+        return urlExists;
     }catch(error){
         console.log("in catch... no file found "+error)
         return false;
@@ -101,8 +172,31 @@ async function updateEmbeddings(url,path){
     createChatBot(url);
     console.log("Updated embeddings");
 }
-//createChatBot("https://daywiseai.com","How does daywiseai help us?");
+createChatBot("https://daywiseai.com","How does daywiseai help us?");
 //chatBotPrompt("https://daywiseai.com","how can daywiseai can help me?");
 //removeEmbeddings(VECTOR_STORE_PATH);
 //updateEmbeddings(url,VECTOR_STORE_PATH);
 module.exports=createChatBot
+
+
+async function generateEmbeddings(text, project_id) {
+    const textSplitter = new RecursiveCharacterTextSplitter({
+      chunkSize: 1000,
+      chunkOverlap: 200,
+    });
+    const documents = await textSplitter.createDocuments([text]);
+    console.log(documents);
+
+    const content = documents.map((doc) => doc.pageContent).join('\n');
+    const metadata = { project_id : project_id };
+    const docs = [new Document({ pageContent: content, metadata })];
+    
+    let vectorStore = await SupabaseVectorStore.fromDocuments(
+      docs,
+      new OpenAIEmbeddings(),
+      {
+        client: SUPABASE_CLIENT,
+        tableName: 'documents',
+      }
+    );
+  }
