f0f57689d19090fc504fabd946ccfa2931ea2a0a
Vedanti.U <vedanti.vvu@gmail.com>
2023-11-17 20:00:17 +0530

    changes in package

--- index.js
+++ index.js
@@ -11,4 +11,5 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-module.exports = app;
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
